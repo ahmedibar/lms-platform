2b0107fda18febb1645203942a4e2500bbdbf445
Ahmed Nur Ali <88971150+ahmedibar@users.noreply.github.com>
2024-03-29 22:13:03 +0530

    Setting up the UI for course creation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import ToastProvider from "@/components/providers/toaster-provider";
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ToastProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
